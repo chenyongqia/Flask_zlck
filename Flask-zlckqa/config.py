2e9793303df0d091b526d55d36dd8866bc596464
陈永奇 <2637781697@qq.com>
2022-08-24 11:05:15 +0800

    本项目是基于知了传课，独立完成的一个问答界面

---
+++ Flask-zlckqa/config.py
@@ -0,0 +1,24 @@
+# 数据库的配置信息
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'zlckqa'
+USERNAME = 'root'
+PASSWORD = '123456'
+DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
+SQLALCHEMY_DATABASE_URI = DB_URI
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+SECRET_KEY = 'sdfsadfskrwerfj1233453345'
+
+
+# 邮箱配置
+# 项目中用的是QQ邮箱
+MAIL_SERVER = 'smtp.qq.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_DEBUG = True
+MAIL_USERNAME = '2637781697@qq.com'
+MAIL_PASSWORD = 'llhtmxtnisiueagf'
+MAIL_DEFAULT_SENDER = '2637781697@qq.com'
